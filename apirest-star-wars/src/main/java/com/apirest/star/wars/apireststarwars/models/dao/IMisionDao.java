daff6a27901cac5de5b4d960a07425d1c7f8005f
joblari92 <80528349+joblari92@users.noreply.github.com>
2022-12-11 21:43:19 +0100

    Delete apirest-star-wars directory

--- apirest-star-wars/src/main/java/com/apirest/star/wars/apireststarwars/models/dao/IMisionDao.java
+++
@@ -1,9 +0,0 @@
-package com.apirest.star.wars.apireststarwars.models.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.apirest.star.wars.apireststarwars.models.entity.Mision;
-
-public interface IMisionDao extends CrudRepository<Mision, Long>{
-
-}
